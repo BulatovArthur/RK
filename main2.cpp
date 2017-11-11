bcb3984f5481a0037c51b44b886be6df54276b1d
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2017-11-11 18:17:23 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -18,22 +18,26 @@ int main() {
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, array);
   length_int = atoi(length.c_str());
-  for (int i = 0; i < array.length(); i++) {
+  for (int i = 0; i < array.length(); i++) 
+  {
     if (array[i] == ' ') counter++;
   }
   if (length_int != (counter + 1))
     cout << " An error has occurred while reading" << endl;
   else {
     getline(cin, count);
-    if (count == "") {
+    if (count == "") 
+    {
       cout << " An error has occurred while reading" << endl;
-    } else {
+    } else 
+      {
       while (k != (length_int - atoi(count.c_str()))) {
         part2 = part2 + array[z];
         if (array[z] == ' ') k++;
         z++;
       }
-      for (int i = part2.length(); i < array.length(); i++) {
+      for (int i = part2.length(); i < array.length(); i++) 
+      {
         part1 = part1 + array[i];
       }
 
