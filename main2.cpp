e876abdae05722be9c6ebf251fed1351981a3ffe
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2017-11-13 13:08:10 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -8,26 +8,33 @@ int main()
     cout << "Count: " << endl;
     cin >> n;
     if (n <= 0)
-        return 0;
+      return 0;
+    
     int a[n];
-     cout << "A number of: " << endl;
-    for (i = 0; i < n; i++) {
-
+    
+    cout << "A number of: " << endl;
+    
+    for (i = 0; i < n; i++) 
+    {
         cin >> a[i];
     }
     cout << "Elements: " << endl;
     cin >> x;
     int b[x - 1], c[n - x - 1];
-    for (i = 0; i < x; i++) {
+    for (i = 0; i < x; i++) 
+    {
         b[i] = a[i];
     }
-    for (i = 0; i < n - x; i++) {
+    for (i = 0; i < n - x; i++) 
+    {
         c[i] = a[i + x];
     }
-    for (i = 0; i < n - x; i++) {
+    for (i = 0; i < n - x; i++) 
+    {
         cout << c[i] << " ";
     }
-    for (i = 0; i < x; i++) {
+    for (i = 0; i < x; i++) 
+    {
         cout << b[i] << " ";
     }
     return 0;
