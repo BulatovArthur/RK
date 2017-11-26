7abdc0a3a6bf28b1a08afd02837db9f0c6595d40
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2017-11-26 23:24:59 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -4,38 +4,57 @@ using namespace std;
 
 int main()
 {
-    int i, n, x;
-    cout << "Count: " << endl;
-    cin >> n;
-    if (n <= 0)
-      return 0;
-    
-    int a[n];
-    
-    cout << "A number of: " << endl;
-    
-    for (i = 0; i < n; i++) 
-    {
-        cin >> a[i];
-    }
-    cout << "Elements: " << endl;
-    cin >> x;
-    int b[x - 1], c[n - x - 1];
-    for (i = 0; i < x; i++) 
-    {
-        b[i] = a[i];
-    }
-    for (i = 0; i < n - x; i++) 
-    {
-        c[i] = a[i + x];
-    }
-    for (i = 0; i < n - x; i++) 
-    {
-        cout << c[i] << " ";
-    }
-    for (i = 0; i < x; i++) 
-    {
-        cout << b[i] << " ";
-    }
-    return 0;
+  int i, n, x;
+  cout << "Введите размер последовательности: " << endl;
+  cin >> n;
+  
+  if (n <= 0)
+  {
+    cout << "error" << endl;
+    return 1;
+  }
+  
+  int *a = new int[n];
+  cout << "Введите элементы последовательности: " << endl;
+  
+  for (i = 0; i < n; i++)
+  {                        
+    cin >> a[i];           // заполнение массива
+  }
+  
+  cout << "После какого элемента разделить последовательность?" << endl;
+  cin >> x;
+  
+  if ( x <= 0)
+  {
+    cout << "error" << endl;
+    return 1;
+  }
+
+  if ( x > n )
+  {
+    cout << "error" << endl;
+    return 1;
+  }
+  
+  int b[x - 1], c[n - x - 1];
+
+  for (i = 0; i < x; i++)
+  {
+    b[i] = a[i];
+  }
+
+  for (i = 0; i < n - x; i++)
+  {
+    c[i] = a[i + x];
+    cout << c[i] << " ";
+  }
+  
+  for (i = 0; i < x; i++)
+  {
+    cout << b[i] << " ";
+  }
+  delete [] a;
+  return 0;
 }
+
