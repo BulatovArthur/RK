7bc3030ef209fbea30c7749e0cd222f1c0113ca3
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2017-11-26 22:41:06 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int b, m;
-  cout << "Count: " << endl;
-  cin >> b;
-  if (b <= 0) {
-    cout << "error" << endl;
+int main()
+{
+  int n, m;
+  cout << "Size" << endl;
+  cin >> n;
+  if ( n <= 0 ) 
+  {
+    cout << "Error" << endl;
+    return 1;
   }
-  int a[b];
-  for (m = 0; m < b; m++) {
+  cout << "Items of arr" << endl;
+  int *a = new int [n];
+  for (m = 0; m < n; m++)
+  {
     cin >> a[m];
   }
-  for (m = 0; m < b / 2; m++) {
-    swap(a[m], a[b - m - 1]);
+  for (m = 0; m < n/2; m++)
+  {
+    swap (a[m], a[n-m-1]);
   }
-  for (m = 0; m < b; m++) {
-    if (b != m) {
+  for (m = 0; m < n; m++)
+  {
+    if(n != m)
+    {
       cout << "error" << endl;
-      return 0;
+      return 1;
     }
     cout << a[m] << endl;
   }
+  delete [] a;
 }
